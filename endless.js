7966251811e5686aa6590cfa13826d69adce51bf
TuoluH <81064670+TuoluH@users.noreply.github.com>
2025-01-22 16:54:29 +0000

    endless works

---
+++ endless.js
@@ -0,0 +1,28 @@
+let endless = document.getElementById("endless");
+let numWordsGuessed = 0;
+
+endless.addEventListener("change", addWordCount);
+
+let finishedWords = document.createElement("div");
+let wordCount = document.createElement("p");
+wordCount.innerHTML = "Word Count: ";
+finishedWords.appendChild(wordCount);
+let guessedWords = document.createElement("p");
+finishedWords.appendChild(guessedWords);
+
+function addWordCount() {
+    if (endless.value == "ON") {
+        document.getElementById("guessResult").after(finishedWords);
+    } else {
+        finishedWords.remove()
+        wordCount.innerHTML = "Word Count: ";
+        guessedWords.innerHTML = "";
+    }
+}
+
+function addWord() {
+    guessedWords.innerHTML += word + ", ";
+    numWordsGuessed++;
+    wordCount.innerHTML = "Word Count: " + numWordsGuessed;
+    let currHighScore = document.getElementById("highScore").innerHTML.substring(document.getElementById("highScore").innerHTML.length - 1);
+}
--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-//global variables here: word, word array(s), guesses, guessedLetters
 let word = "";
 let guessedLetters = [];
 let numWrongGuessesLeft;
 let currWord = "";
-//event listeners for startGame and guessLetter
+
 window.onload = startGame()
 document.getElementById("start").addEventListener("click", startGame);
 document.getElementById("guessLetter").addEventListener("click", guessLetter);
 document.getElementById("guessWord").addEventListener("click", guessWord);
 document.getElementById("guess").addEventListener("keyup", checkKey);
-//once at start of the game
+
 function startGame() {
     document.getElementById("guess").value = "",
     document.getElementById("guessedLetters").innerHTML = "";
@@ -18,25 +17,18 @@ function startGame() {
     guessedLetters = [];
     currWord = ""
     word = words[Math.floor(Math.random() * words.length)];
+    console.log(word);
     document.getElementById("guess").setAttribute("maxlength", word.length);
-    numWrongGuessesLeft = 8;
-    document.getElementById("numGuesses").innerHTML = "Guesses Remaining: " + numWrongGuessesLeft;
+    if (endless.value == "OFF") {
+        numWrongGuessesLeft = 8;
+        document.getElementById("numGuesses").innerHTML = "Guesses Remaining: " + numWrongGuessesLeft;
+    }
     for (let i = 0; i < word.length; i++) {
         currWord += "_ "
     }
     document.getElementById("word").innerHTML = currWord;
-    // document.getElementById("word").innerHTML = word;
-    /*
-    - Reset the board, empty guessedLetters 
-    - Set a word from words array into word - this line will grab  a random element from your words array for you:
-    word = words[Math.floor(Math.random() * words.length)];
-    */
-
 }
 
-//at start and every time the user enters a guess
-
-//every time the user enters a guess
 function guessLetter() {
     let letter = document.getElementById("guess").value.toLowerCase();
     if (!checkGuess(letter)) {
@@ -52,27 +44,27 @@ function guessLetter() {
             if (letter == word.charAt(g)) {
                 currWord = currWord.substring(0, g * 2) + letter + currWord.substring(g * 2 + 1);
             }
-            if (currWord.replaceAll(" ", "") == word) {
+            if (currWord.replaceAll(" ", "") == word && endless.value == "OFF") {
                 gameOver(true);
+            } else if (currWord.replaceAll(" ", "") == word) {
+                addWord();
+                startGame();
             }
         }
     }
 
-    /*
-    Manage the game: Add letters to guessedLetters, call printWord, deduct from guesses, check for a win or loss.
-    */
     document.getElementById("word").innerHTML = currWord;
     document.getElementById("guess").value = "";
 }
 
 function checkGuess(guess) {
-    if ("abcdefghijklmnopqrstuvwxyz".indexOf(guess) == -1) {
-        // checks if guess is letter
-        document.getElementById("guessResult").innerHTML = "Guess a Letter";
-        setTimeout(clearResult, 1000);
-    } else if (guess.length > 1) {
+    if (guess.length > 1) {
         // checks if guess is only one letter
         document.getElementById("guessResult").innerHTML = "Only Guess One Letter At a Time";
+        setTimeout(clearResult, 1000);
+    } else if ("abcdefghijklmnopqrstuvwxyz".indexOf(guess) == -1) {
+        // checks if guess is letter
+        document.getElementById("guessResult").innerHTML = "Guess a Letter";
         setTimeout(clearResult, 1000);
     } else if (guessedLetters.includes(guess)) {
         // checks if letter has already been guessed
@@ -80,17 +72,23 @@ function checkGuess(guess) {
         setTimeout(clearResult, 1000);
     } else if (guess.length == 1 && guess != "" && word.includes(guess)) {
         return true;
-    } else {
+    } else if (guess != "") {
         return false;
 
     }
+    return "do nothing"
 }
 
 function guessWord() {
     let guess = document.getElementById("guess").value;
     if (guess.toLowerCase() == word) {
         document.getElementById("word").innerHTML = word.split("").join(" ");
-        gameOver(true);
+        if (endless.value == "OFF") {
+            gameOver(true);
+        } else {
+            addWord();
+            startGame();
+        }
     } else {
         numWrongGuessesLeft -= 1;
         document.getElementById("numGuesses").innerHTML = "Guesses Remaining: " + numWrongGuessesLeft;
@@ -115,9 +113,9 @@ function gameOver(win) {
 
 function checkKey(e) {
     let guessLen = document.getElementById("guess").value.length;
-    if (e.code == "Enter" && guessLen == 1) {
+    if (e.code == "Enter" && guessLen < word.length) {
         guessLetter();
-    } else if (e.code == "Enter" && guessLen > 1) {
+    } else if (e.code == "Enter" && guessLen == word.length) {
         guessWord();
     }
 }
